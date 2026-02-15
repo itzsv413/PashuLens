9d10a9cab7b7366aafca034deea9e585a503bebe
itzsv413 <sv809413@gmail.com>
2026-02-15 20:16:17 +0530

    Connected frontend to live backend

--- src/api/mlApi.js
+++ src/api/mlApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const ML_API = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://pashulens.onrender.com", //To run locally use http://127.0.0.1:8000
 });
 
 export default ML_API;
